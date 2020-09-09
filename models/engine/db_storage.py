e59a7d609b028b024c2f09f765ceadb0afaec76f
fdetun <fouedads@gmail.com>
2020-09-09 22:24:51 +0100

    m

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,8 +58,6 @@ class DBStorage:
         for i in self.all(cls).values():
             if i.id == id:
                 return i
-            else:
-                return None
 
     def count(self, cls=None):
         """count db"""
---
+++ shell.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+service mysql start;
+
+rm -rf file.json;
+rm -rf dev/file.json;
+
+sleep 1;
+
+echo "DROP USER IF EXISTS 'hbnb_dev'@'localhost';" | mysql -ulocalhost -uroot -ptest;
+echo "DROP DATABASE IF EXISTS hbnb_dev_db;" | mysql -ulocalhost -uroot -ptest;
+
+sleep 1;
+
+echo "CREATE DATABASE IF NOT EXISTS hbnb_dev_db;" | mysql -ulocalhost -uroot -ptest;
+echo "CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';" | mysql -ulocalhost -uroot -ptest;
+echo "SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';" | mysql -ulocalhost -uroot -ptest;
+echo "GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';" | mysql -ulocalhost -uroot -ptest;
+echo "GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';" | mysql -ulocalhost -uroot -ptest;
+echo "FLUSH PRIVILEGES;" | mysql -ulocalhost -uroot -ptest;
+
+sleep 1;
+
+export HBNB_ENV=dev;
+export HBNB_MYSQL_USER=hbnb_dev;
+export HBNB_MYSQL_PWD=hbnb_dev_pwd;
+export HBNB_MYSQL_HOST=localhost;
+export HBNB_MYSQL_DB=hbnb_dev_db;
+export HBNB_TYPE_STORAGE=db;
+
+echo 'create State name="Arizona"' | ./console.py ;
+echo 'create State name="California"' | ./console.py ;
+echo 'create State name="Louisiana"' | ./console.py ;
+echo 'create State name="Texas"' | ./console.py ;
+
