c8f43f19cdd813149a36200268a57bc0469ec545
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-09 20:48:32 -0400

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,37 @@
 """task 8"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 app = Flask(__name__)
+
+
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """status"""
+    d = {}
+    for cls in classes:
+        print(cls)
+        a = storage.count(cls)
+        d[cls] = a
+    return jsonify(d)
 
 
 if __name__ == "__main__":
