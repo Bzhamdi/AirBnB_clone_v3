db66a4b0e3261c537862ef673b5d7ede3584dfb8
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-09 21:37:10 -0400

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,18 +3,24 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
+from models.user import User
 
 
 app = Flask(__name__)
 
 
 classes = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
 }
 
 
@@ -24,13 +30,12 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats',  methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def stats():
     """status"""
     d = {}
-    for cls in classes:
-        print(cls)
-        a = storage.count(cls)
+    for cls, value in classes.items():
+        a = storage.count(value)
         d[cls] = a
     return jsonify(d)
 
